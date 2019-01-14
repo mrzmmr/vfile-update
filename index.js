274944e9016bab8ee32de1a105a5c7a0a2a6d082
mrzmmr <mrzmmr@gmail.com>
2019-01-14 21:47:41 +0000

    Refactor code style

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 'use strict';
 
-var visit = require('vfile-visit');
+const visit = require('vfile-visit');
 
 module.exports = update;
 module.exports.undo = undo;
 
 function update(file, callback) {
-  if (typeof callback !== 'function') {
-    callback = function () {};
-  }
+	if (typeof callback !== 'function') {
+		callback = function () {};
+	}
 
-  return visit(file, function (current, parent, index) {
-    if (current.path && parent.path) {
-      current.dirname = parent.path;
-    }
-    callback(current, parent, index);
-  });
+	return visit(file, (current, parent, index) => {
+		if (current.path && parent.path) {
+			current.dirname = parent.path;
+		}
+
+		callback(current, parent, index);
+	});
 }
 
 function undo(file) {
-  return visit(file, function (current) {
-    if (current.history.length > 1) {
-      current.history.pop();
-    }
-  });
+	return visit(file, current => {
+		if (current.history.length > 1) {
+			current.history.pop();
+		}
+	});
 }
--- test.js
+++ test.js
@@ -1,95 +1,84 @@
 'use strict';
 
-var {test} = require('tap');
-var clone = require('clone');
-var vfile = require('vfile');
-var update = require('./');
+const {test} = require('tap');
+const clone = require('clone');
+const vfile = require('vfile');
+const update = require('.');
 
-var file = vfile({
-  path: 'foo',
-  contents: [
-    vfile({
-      path: 'bar',
-      contents: [
-        vfile({
-          path: 'bar.txt',
-          contents: 'Bar'
-        })
-      ]
-    }),
-    vfile({
-      path: 'foo.txt',
-      contents: 'Foo'
-    })
-  ]
+const file = vfile({
+	path: 'foo',
+	contents: [
+		vfile({
+			path: 'bar',
+			contents: [
+				vfile({
+					path: 'bar.txt',
+					contents: 'Bar'
+				})
+			]
+		}),
+		vfile({
+			path: 'foo.txt',
+			contents: 'Foo'
+		})
+	]
 });
 
-test('cloned copy !== file', function (t) {
-  var copy = clone(file);
-  t.ok(copy !== file);
-  t.end();
+test('cloned copy !== file', t => {
+	const copy = clone(file);
+	t.ok(copy !== file);
+	t.end();
 });
 
-test('undo - file', function (t) {
-  var copy = clone(file);
-  var updated = update(copy);
-  t.deepEqual(file, update.undo(updated));
-  t.end();
+test('undo - file', t => {
+	const copy = clone(file);
+	const updated = update(copy);
+	t.deepEqual(file, update.undo(updated));
+	t.end();
 });
 
-test('update - file', function (t) {
-  var copy = clone(file);
-  copy.contents[0].path = 'foo/bar';
-  copy.contents[1].path = 'foo/foo.txt';
-  copy.contents[0].contents[0].path = 'foo/bar/bar.txt';
-  t.deepEqual(copy, update(file));
-  t.end();
+test('update - file', t => {
+	const copy = clone(file);
+	copy.contents[0].path = 'foo/bar';
+	copy.contents[1].path = 'foo/foo.txt';
+	copy.contents[0].contents[0].path = 'foo/bar/bar.txt';
+	t.deepEqual(copy, update(file));
+	t.end();
 });
 
-test('undo - if theres no contents then skip', function (t) {
-  var copy = clone(file);
-  delete copy.contents[0].contents;
-  t.ok(update.undo(copy));
-  t.end();
+test('undo - if theres no contents then skip', t => {
+	const copy = clone(file);
+	delete copy.contents[0].contents;
+	t.ok(update.undo(copy));
+	t.end();
 });
 
-test('update - if theres no contents then skip', function (t) {
-  var copy = clone(file);
-  delete copy.contents[0].contents;
-  t.ok(update(copy));
-  t.end();
+test('update - if theres no contents then skip', t => {
+	const copy = clone(file);
+	delete copy.contents[0].contents;
+	t.ok(update(copy));
+	t.end();
 });
 
-test('update - if history.length === 0, then skip', function (t) {
-  var copy = clone(file);
-  copy.contents[0].history = [];
-  t.ok(update(copy));
-  t.end();
+test('update - if history.length === 0, then skip', t => {
+	const copy = clone(file);
+	copy.contents[0].history = [];
+	t.ok(update(copy));
+	t.end();
 });
 
-test('undo - if history.length === 0, then skip', function (t) {
-  var copy = clone(file);
-  copy.contents[0].history = [];
-  t.ok(update.undo(copy));
-  t.end();
+test('undo - if history.length === 0, then skip', t => {
+	const copy = clone(file);
+	copy.contents[0].history = [];
+	t.ok(update.undo(copy));
+	t.end();
 });
 
-test('update - callback', function (t) {
-  var foo = vfile({path: 'foo'})
-  var bar = update(foo, function (current) {
-    current.path = 'bar'
-    t.notSame(current, foo)
-  })
-  t.end()
-})
-
-// test('update - callback', function (t) {
-//   var copy = clone(file);
-//   update(copy, function (node, parent) {
-//     t.ok(node !== copy);
-//     if (parent && parent.contents) {
-//       t.ok(parent.contents.indexOf(node) > -1);
-//     }
-//   });
-//   t.end();
-// });
+test('update - callback', t => {
+	const foo = vfile({path: 'foo'});
+	update(foo, current => {
+		current.path = 'bar';
+		t.notSame(current, foo);
+	});
+	t.end();
+});
